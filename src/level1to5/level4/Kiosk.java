07771bfcabf344313822f98dae97254d6b41110c
jhyang22 <jaeho9229@gmail.com>
2025-03-10 20:07:36 +0900

    뒤로가기는 완료. 단계적 가리킴이 가능한 코드를 위해 고민 중

--- src/level1to5/level4/Kiosk.java
+++ src/level1to5/level4/Kiosk.java
@@ -5,52 +5,66 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
-    private List<MenuItem> menuItemList = new ArrayList<>();
+    //    private Menu menu = new Menu();
+    private Menu menu = new Menu();
+    // 카테고리 리스트를 만들어야 하나?
 
     Scanner scanner = new Scanner(System.in);
 
-    public Kiosk() {
-        menuItemList.add(new MenuItem("ClassicBurger", 7.9, "아시죠? 기본이 최곤거! 클래식 버거가 여기있습니다"));
-        menuItemList.add(new MenuItem("CheeseBurger", 10.9, "진한 육미가 느껴지는 소고기패티를 치즈가 감싸고 있는 치즈버거"));
-        menuItemList.add(new MenuItem("BaconBurger", 12.9, "얇은 베이컨? ㄴㄴ 두께가 있어 식감이 좋은 진퉁 베이컨이 들어간 치즈버거"));
-        menuItemList.add(new MenuItem("MegaBurger", 16.9, "햄최몇들 다 드루와"));
-    }
 
+    // 먼저 카테고리 목록 나와야함
+    // 카테고리를 선택할 경우 그 카테고리에 해당하는 menuItemList가 나와야함
+    // -> 0번째 인덱스엔 categoryList, 1번째 인덱스엔 menuItemList를 넣을수 있나?
+    // -> 근데 그렇게 하면 안될것같은데.. 카테고리마다 지정을 해야할거같은데 어떻게하지
+    // 0을 누를경우 뒤로가기 혹은 종료가 나와야함
     public void start() {
+        int categoryChoose = -1;
         int menuChoose = -1;
-        while (!(menuChoose == 0)) {
-            System.out.println(" ");
-            System.out.println("------------------------------------------------MENU------------------------------------------------");
 
+        // if categoryChoose == 0 이면 종료하기, menuChoose가 0이면 뒤로가기, 0~4 사이가 아니면 다시입력, 문자열은 try-catch로 다시입력
+        while (!(categoryChoose == 0)) {
+
+            System.out.println(" ");
+            System.out.println("---------------------------------------------MAIN MENU---------------------------------------------");
             int i = 1;
-            for (MenuItem a : menuItemList) {
-                System.out.println(i + ". " + a.getName() + "      |    W " + a.getPrice() + "   |   " + a.getExplanation());
+            for (String a : menu.getCategory()) {
+                System.out.println(i + ". " + a);
                 i++;
             }
+            System.out.println("0. 종료하기");
+            System.out.println("----------------------------------------------------------------------------------------------------");
 
-            System.out.println("0. 종료");
-            System.out.println(" ");
-            System.out.print("번호를 선택하세요: ");
-            while(true) {
-                try {
+            // 카테고리 고르기
+            System.out.print("카테고리를 선택하세요: ");
+            categoryChoose = scanner.nextInt();
+            i = 1;
+            switch (categoryChoose) {
+                case 1:
+                    System.out.println("-----------------------------------------BURGER MENU-------------------------------------------");
+                    for (MenuItem a : menu.getBurgerList()) {
+                        System.out.println(i + ". " + a.getName() + "      |    W " + a.getPrice() + "   |   " + a.getExplanation());
+                        i++;
+                    }
+                    System.out.println("0. 뒤로가기");
+                    System.out.println("----------------------------------------------------------------------------------------------------");
+
+                    // 메뉴 고르기
+                    System.out.print("번호를 선택하세요: ");
                     menuChoose = scanner.nextInt();
-                    while (true) {
-                        if (menuChoose >= 0 && menuChoose <= 4) {
+                    while (!(menuChoose == 0)) {
+                        if (menuChoose >= 1 && menuChoose <= 4) {
                             switch (menuChoose) {
                                 case 1:
-                                    System.out.println("선택한 메뉴 : " + menuItemList.get(0).getName() + ", " + menuItemList.get(0).getPrice() + "W, " + menuItemList.get(0).getExplanation());
+                                    System.out.println("선택한 메뉴 : " + menu.getBurgerList().get(0).getName() + ", " + menu.getBurgerList().get(0).getPrice() + "W, " + menu.getBurgerList().get(0).getExplanation());
                                     break;
                                 case 2:
-                                    System.out.println("선택한 메뉴 : " + menuItemList.get(1).getName() + ", " + menuItemList.get(1).getPrice() + "W, " + menuItemList.get(1).getExplanation());
+                                    System.out.println("선택한 메뉴 : " + menu.getBurgerList().get(1).getName() + ", " + menu.getBurgerList().get(1).getPrice() + "W, " + menu.getBurgerList().get(1).getExplanation());
                                     break;
                                 case 3:
-                                    System.out.println("선택한 메뉴 : " + menuItemList.get(2).getName() + ", " + menuItemList.get(2).getPrice() + "W, " + menuItemList.get(2).getExplanation());
+                                    System.out.println("선택한 메뉴 : " + menu.getBurgerList().get(2).getName() + ", " + menu.getBurgerList().get(2).getPrice() + "W, " + menu.getBurgerList().get(2).getExplanation());
                                     break;
                                 case 4:
-                                    System.out.println("선택한 메뉴 : " + menuItemList.get(3).getName() + ", " + menuItemList.get(3).getPrice() + "W, " + menuItemList.get(3).getExplanation());
-                                    break;
-                                default:
-                                    System.out.println("프로그램을 종료합니다");
+                                    System.out.println("선택한 메뉴 : " + menu.getBurgerList().get(3).getName() + ", " + menu.getBurgerList().get(3).getPrice() + "W, " + menu.getBurgerList().get(3).getExplanation());
                                     break;
                             }
                             System.out.println("----------------------------------------------------------------------------------------------------");
@@ -60,13 +74,68 @@ public class Kiosk {
                             menuChoose = scanner.nextInt();
                         }
                     }
+                    if (menuChoose == 0) {
+                        continue;
+                    } else {
+                        break;
+                    }
+                case 2:
+                    System.out.println("-----------------------------------------DRINK MENU-------------------------------------------");
+                    for (MenuItem a : menu.getDrinkList()) {
+                        System.out.println(i + ". " + a.getName() + "      |    W " + a.getPrice() + "   |   " + a.getExplanation());
+                        i++;
+                    }
+                    System.out.println("0. 뒤로가기");
+                    System.out.println("----------------------------------------------------------------------------------------------------");
+                    System.out.print("번호를 선택하세요: ");
+                    menuChoose = scanner.nextInt();
+                    switch (menuChoose) {
+                        case 1:
+                            System.out.println("선택한 메뉴 : " + menu.getDrinkList().get(0).getName() + ", " + menu.getDrinkList().get(0).getPrice() + "W, " + menu.getDrinkList().get(0).getExplanation());
+                            break;
+                        case 2:
+                            System.out.println("선택한 메뉴 : " + menu.getDrinkList().get(1).getName() + ", " + menu.getDrinkList().get(1).getPrice() + "W, " + menu.getDrinkList().get(1).getExplanation());
+                            break;
+                        case 3:
+                            System.out.println("선택한 메뉴 : " + menu.getDrinkList().get(2).getName() + ", " + menu.getDrinkList().get(2).getPrice() + "W, " + menu.getDrinkList().get(2).getExplanation());
+                            break;
+                        case 4:
+                            System.out.println("선택한 메뉴 : " + menu.getDrinkList().get(3).getName() + ", " + menu.getDrinkList().get(3).getPrice() + "W, " + menu.getDrinkList().get(3).getExplanation());
+                            break;
+                    }
+                    System.out.println("----------------------------------------------------------------------------------------------------");
                     break;
-                } catch (RuntimeException e) {
-                    System.out.print("잘못 입력하였습니다. 0~4까지의 숫자를 입력해주세요: ");
-                    scanner.nextLine();
-                }
+                case 3:
+                    System.out.println("-----------------------------------------SIDE MENU-------------------------------------------");
+                    for (MenuItem a : menu.getSideList()) {
+                        System.out.println(i + ". " + a.getName() + "      |    W " + a.getPrice() + "   |   " + a.getExplanation());
+                        i++;
+                    }
+                    System.out.println("0. 뒤로가기");
+                    System.out.println("----------------------------------------------------------------------------------------------------");
+                    System.out.print("번호를 선택하세요: ");
+                    menuChoose = scanner.nextInt();
+                    switch (menuChoose) {
+                        case 1:
+                            System.out.println("선택한 메뉴 : " + menu.getSideList().get(0).getName() + ", " + menu.getSideList().get(0).getPrice() + "W, " + menu.getSideList().get(0).getExplanation());
+                            break;
+                        case 2:
+                            System.out.println("선택한 메뉴 : " + menu.getSideList().get(1).getName() + ", " + menu.getSideList().get(1).getPrice() + "W, " + menu.getSideList().get(1).getExplanation());
+                            break;
+                        case 3:
+                            System.out.println("선택한 메뉴 : " + menu.getSideList().get(2).getName() + ", " + menu.getSideList().get(2).getPrice() + "W, " + menu.getSideList().get(2).getExplanation());
+                            break;
+                        case 4:
+                            System.out.println("선택한 메뉴 : " + menu.getSideList().get(3).getName() + ", " + menu.getSideList().get(3).getPrice() + "W, " + menu.getSideList().get(3).getExplanation());
+                            break;
+                    }
+                    System.out.println("----------------------------------------------------------------------------------------------------");
+                    break;
             }
-            System.out.println(" ");
         }
+        System.out.println("프로그램을 종료합니다");
     }
 }
+
+// 그러니까 menuList.get카테고리.get그에 맞는 메뉴리스트.get속성; 이런식으로 들어가고싶은데 지금은 그렇게 안돼있음
+// 예를들어 menuList.getBurgers.getCokeZero는 안되게!
--- src/level1to5/level4/Main.java
+++ src/level1to5/level4/Main.java
@@ -2,10 +2,15 @@ package level1to5.level4;
 
 public class Main {
     public static void main(String[] args) {
+
+
         Kiosk kiosk = new Kiosk();
         kiosk.start();
     }
 }
 
-
 // Menu 클래스 생성
+
+
+// Menu 객체 생성하면서 카테고리 이름 설정
+// Menu 클래스 내 있는 List<MenuItem>에 MenuItem 객체 생성하면서 삽입
